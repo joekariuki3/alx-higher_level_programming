533e8ec603ad5a8804126af94b86200c155440a4
joekariuki3 <joekariuki3@gmail.com>
2023-06-09 15:19:40 +0300

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        index = 0
+        while index < len(my_list):
+            new_list.append(my_list[index])
+            index = index + 1
+        new_list[idx] = element
+        return new_list
