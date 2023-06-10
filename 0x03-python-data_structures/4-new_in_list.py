f6c75f302659071bf0a328b3d8258a5cb196920c
joekariuki3 <joekariuki3@gmail.com>
2023-06-10 18:29:30 +0300

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = []
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
         index = 0
