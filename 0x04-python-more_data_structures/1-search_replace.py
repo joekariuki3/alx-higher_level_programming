ce73f1cf844e53c227fd1cfae0cea911f4149d60
joekariuki3 <joekariuki3@gmail.com>
2023-06-14 12:33:22 +0300

    Number of keys

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,8 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return None
+    elif not search or not replace:
+        return (my_list)
     new_list = []
     for num in my_list:
         if num == search:
