cc42f2be886234b15d03026b6612fcac95c75228
joekariuki3 <joekariuki3@gmail.com>
2023-06-14 19:24:32 +0300

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return 0
     pairTotal = 0
     toDivWith = 0
-    for pair in my_list: 
+    for pair in my_list:
         pairTotal = pairTotal + (pair[0] * pair[1])
         toDivWith = toDivWith + pair[1]
     return pairTotal / toDivWith
