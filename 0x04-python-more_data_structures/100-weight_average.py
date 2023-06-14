9a41676d442862bd18ce0b414c699d1c95ad044e
joekariuki3 <joekariuki3@gmail.com>
2023-06-14 19:12:01 +0300

    Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == None:
+        return 0
+    pairTotal = 0
+    toDivWith = 0
+    for pair in my_list:
+        pairTotal = pairTotal + (pair[0] * pair[1])
+        toDivWith = toDivWith + pair[1]
+    return pairTotal / toDivWith
