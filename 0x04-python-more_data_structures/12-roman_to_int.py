c72a455cd538961f9133c347ec182069da4e170a
joekariuki3 <joekariuki3@gmail.com>
2023-06-14 16:20:19 +0300

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str) and not roman_string:
+        return 0
+    else:
+        values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        for char in roman_string:
+            if char in values.keys():
+                result = result + values[char]
+        return result
