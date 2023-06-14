2bf9cda06bb3440b6f533de45306ff610ef20547
joekariuki3 <joekariuki3@gmail.com>
2023-06-14 12:17:47 +0300

    Revert "Search and replace"
    This reverts commit b1ef6dcfa339e7769f88bde638bb13bc2171ad02.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,10 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return None
-    return [replace if n == search  else n for n in my_list]
+    new_list = []
+    for num in my_list:
+        if num == search:
+            new_list.append(replace)
+        else:
+            new_list.append(num)
+    return new_list
