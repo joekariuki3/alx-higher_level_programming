14a3b9a799e01072b471456eb1cf072f1fd33be0
joekariuki3 <joekariuki3@gmail.com>
2023-06-27 17:34:51 +0300

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This is a square module
+it provides a simple square class
+"""
 class Square:
     """ class square has defination of it self and does nothing """
     def __init__(self):
