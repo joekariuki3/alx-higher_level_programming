9376200b4daea9cc9714985836d3b5a7057aaa7f
joekariuki3 <joekariuki3@gmail.com>
2023-06-27 17:57:23 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a square module
+it provides a simple square class
+"""
+
+
+class Square:
+    """ class square has defination of it self and does nothing """
+    def __init__(self, size):
+        self.__size = size
+        pass
