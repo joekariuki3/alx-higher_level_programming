d5f1b264d6b2075f8bbf225337006a2316337f00
joekariuki3 <joekariuki3@gmail.com>
2023-07-01 18:22:00 +0300

    Divide a matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     if a is not a is float convert to int
     if b is not a is float convert to int
     """
-    if isinstance(a, (int, float)) == False:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif isinstance(b, (int, float)) == False:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     if type(a) == float:
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,13 +10,13 @@ def matrix_divided(matrix, div):
         for row in matrix:
             if len(row) != row_size:
                 raise TypeError("Each row of the matrix must have the same size")
-            elif isinstance(row, list) == False:
+            elif not isinstance(row, list):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             for num in row:
-                if isinstance(num, (int, float)) == False:
+                if not isinstance(num, (int, float)):
                     raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if  isinstance(div, (int, float)) == False:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return list(map(lambda row: list(map(lambda n: round((n / div),2), row)), matrix))
+    return list(map(lambda row: list(map(lambda n: round((n / div), 2), row)), matrix))
