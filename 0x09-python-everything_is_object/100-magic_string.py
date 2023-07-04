7891b947b895221ad1a77d4d276ff9981b7af697
joekariuki3 <joekariuki3@gmail.com>
2023-07-04 22:25:37 +0300

    refactor magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
-    return ", ".join(["BestSchool" for i in range(magic_string.counter)])
+def magic_string(num=[]):
+    num.append("x")
+    return("BestSchool, " * (len(num) - 1) + "BestSchool")
