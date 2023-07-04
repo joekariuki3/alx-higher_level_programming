5ad0fa8b8789fac174ccfa3d4b7557fbe95ce27e
joekariuki3 <joekariuki3@gmail.com>
2023-07-04 22:27:49 +0300

    refactor magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(num=[]):
     num.append("x")
-    return("BestSchool, " * (len(num) - 1) + "BestSchool")
+    return ("BestSchool, " * (len(num) - 1) + "BestSchool")
