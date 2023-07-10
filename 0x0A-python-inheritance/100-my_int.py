a85e341c24f8a04bd383cefe2f342844c9d0047f
joekariuki3 <joekariuki3@gmail.com>
2023-07-10 22:03:45 +0300

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-simple MyInt class
-inherits from int
+simple MyInt class it inherits from int
 """
 
 
 class MyInt(int):
+    """
+    call super fuctions to be used with our
+    custom functions
+    """
     def __eq__(self, num):
         """ check for equality using super function"""
         return super().__eq__(num)
