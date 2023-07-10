75713adb4783b28ed2c4bb02f427d5a0d9ee6743
joekariuki3 <joekariuki3@gmail.com>
2023-07-10 13:10:07 +0300

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+lookup contets of a class
+function to check available methods
+and attributes of a class
+"""
+
+
 def lookup(obj):
     """
-    function to check available methods
-    and attributes of a class
+    returns a list of all the methods
+    and atributes of class
     """
     return list(dir(obj))
