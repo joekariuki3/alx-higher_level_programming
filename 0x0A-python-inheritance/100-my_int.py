6b6f0c439732996edb8d0f565825cb5a8458c862
joekariuki3 <joekariuki3@gmail.com>
2023-07-10 22:00:21 +0300

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-simple MyInt class 
+simple MyInt class
 inherits from int
 """
+
+
 class MyInt(int):
     def __eq__(self, num):
+        """ check for equality using super function"""
         return super().__eq__(num)
 
     def __ne__(self, num):
+        """ checks for not equal to using super function """
         return super().__ne__(num)
