3a288c714f4baa9265391f51a5c09d0da96431de
joekariuki3 <joekariuki3@gmail.com>
2023-07-10 16:31:29 +0300

    mylist

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -17,4 +17,8 @@ class MyList(list):
     in assending order
     """
     def print_sorted(self):
+        if not isinstance(self, list):
+            raise TypeError("provide a list")
+        elif self is None:
+            raise TypeError("provide a list")
         print(sorted(self, reverse=False))
