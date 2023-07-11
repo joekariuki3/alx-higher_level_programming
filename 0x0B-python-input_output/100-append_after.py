f6c8ed21d0cee0196958fc705668833aa23fe7ac
joekariuki3 <joekariuki3@gmail.com>
2023-07-11 23:17:47 +0300

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,6 +6,10 @@ after each line containing a specific string
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """
+    look for a string in a line if present
+    append new_string after it
+    """
     if filename != "" or search_string != "" or new_string != "":
         with open(filename, mode="r", encoding="utf-8") as myfile:
             text = myfile.readlines()
