d1613bb1ef57db61ef6c38dbc4bcf42aa30a8826
joekariuki3 <joekariuki3@gmail.com>
2023-07-14 13:29:26 +0300

    Validate attributes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,6 +19,7 @@ class TestRectangle(unittest.TestCase):
         cls.r1 = Rectangle(10, 2)
         cls.r2 = Rectangle(2, 10)
         cls.r3 = Rectangle(10, 2, 0, 0, 12)
+        cls.r4 = Rectangle(3, 7)
 
     """test id None"""
     def test_None(self):
@@ -28,6 +29,46 @@ class TestRectangle(unittest.TestCase):
     """test id with int"""
     def test_int(self):
         self.assertEqual(TestRectangle.r3.id, 12)
+
+    """test width value """
+    def test_width_nonInt(self):
+        with self.assertRaises(TypeError):
+            TestRectangle.r4.width = "5"
+
+    """test width value -ve """
+    def test_width_negative(self):
+        with self.assertRaises(ValueError):
+            TestRectangle.r4.width = -1
+
+    """test height value """
+    def test_height_nonInt(self):
+        with self.assertRaises(TypeError):
+            TestRectangle.r4.height = "5"
+
+    """test height value -ve """
+    def test_height_negative(self):
+        with self.assertRaises(ValueError):
+            TestRectangle.r4.height = -1
+
+    """test x value """
+    def test_x_nonInt(self):
+        with self.assertRaises(TypeError):
+            TestRectangle.r3.x = "5"
+
+    """test x value -ve """
+    def test_x_negative(self):
+        with self.assertRaises(ValueError):
+            TestRectangle.r3.x = -1
+
+    """test y value """
+    def test_y_nonInt(self):
+        with self.assertRaises(TypeError):
+            TestRectangle.r3.y = "5"
+
+    """test y value -ve """
+    def test_y_negative(self):
+        with self.assertRaises(ValueError):
+            TestRectangle.r3.y = -1
 
 
 if __name__ == "__main__":
