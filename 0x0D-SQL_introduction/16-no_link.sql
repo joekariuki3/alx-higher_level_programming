139f8c1f83b90a0009a177f99d18fab2e349b336
joekariuki3 <joekariuki3@gmail.com>
2023-08-15 18:27:16 +0300

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- sccript that list all records in second_table that has name order by scpre
-SELECT score, name FROM second_table WHERE name ORDER BY score DESC
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
