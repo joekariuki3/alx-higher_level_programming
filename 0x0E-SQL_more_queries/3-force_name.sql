589bca9bc6fb6b2a873b4c7ab6f1a171fecad8aa
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 19:41:15 +0300

    remove ' from files

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Script to creat table 'force_name' if not exist
 -- desc id INT, name VARCHAR(256) NOT NULL
-CREATE TABLE IF NOT EXISTS 'force_name' (
+CREATE TABLE IF NOT EXISTS force_name (
 	id INT, name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- create table 'id_not_null' IF NOT EXIST
 -- desc id INT DEFAULT 1, name VARCHAR(256)
-CREATE TABLE IF NOT EXIST 'id_not_null' (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXIST id_not_null (id INT DEFAULT 1, name VARCHAR(256));
