3364e0d1860025b8e596d891093bcd13d962bb1d
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 19:43:51 +0300

    rectify task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,7 +4,7 @@
 -- If the user user_0d_1 already exists, your script should not fail
 DROP USER IF EXISTS user_0d_1@localhost;
 CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd;
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.*
 TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
