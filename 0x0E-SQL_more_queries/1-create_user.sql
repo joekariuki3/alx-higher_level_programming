52be6ea14efe06e479de807b3e5be65d55e2ac0d
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 18:56:17 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,8 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.*
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
