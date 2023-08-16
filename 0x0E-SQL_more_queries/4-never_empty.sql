49145499e8c42001b9dffe66e72992838976303c
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 19:47:44 +0300

    ID can't be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,4 @@
 -- create table 'id_not_null' IF NOT EXIST
 -- desc id INT DEFAULT 1, name VARCHAR(256)
-CREATE TABLE IF NOT EXIST id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXIST id_not_null (
+	id INT DEFAULT 1, name VARCHAR(256));
