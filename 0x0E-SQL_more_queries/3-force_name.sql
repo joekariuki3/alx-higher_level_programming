976dbdd50e20fc59981abe24c956cfde972d8fcf
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 19:19:34 +0300

    Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 --- Script to creat table 'force_name' if not exist
 --- desc id INT, name VARCHAR(256) NOT NULL
+DROP TABLE IF EXISTS force_name;
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT, name VARCHAR(256) NOT NULL);
