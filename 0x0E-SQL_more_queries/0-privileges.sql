fbdce022323ebab801136f79bc124cdbbd1fd58e
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 18:59:26 +0300

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- script to list privilages of users
 -- that have been granted
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
