2178fd7c7da1f155e30825ed01a63a54fa00ed03
joekariuki3 <joekariuki3@gmail.com>
2023-08-16 18:52:33 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,5 @@
 -- If the user user_0d_1 already exists, your script should not fail
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
