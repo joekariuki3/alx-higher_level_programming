1d1cc0a37ecd884b5eb15faa34e69ba916ba0858
joekariuki3 <joekariuki3@gmail.com>
2023-09-11 15:05:36 +0300

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,10 +6,14 @@ if (isNaN(argument)) {
   console.log('Missing size');
 } else {
   const argumentInt = parseInt(argument);
+  let square = '';
   for (let row = 0; row < argumentInt; row++) {
     for (let cell = 0; cell < argumentInt; cell++) {
-      process.stdout.write('x');
+      square = square + 'x';
     }
-    console.log();
+    if (row < argumentInt - 1) {
+      square = square + '\n';
+    }
   }
+  console.log(square);
 }
