2ce459c07ec64b882c01a12f80dd2ddfa2373b97
joekariuki3 <joekariuki3@gmail.com>
2023-09-12 13:11:56 +0300

    Comment on file

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+// rectangle class
+
 class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
+
+// Export the class to be used ouside the file
 module.exports = Rectangle;
