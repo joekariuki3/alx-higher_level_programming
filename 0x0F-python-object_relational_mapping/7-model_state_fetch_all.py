cac07eecf0cb942b234c7d2e080eb54ba3d6a297
joekariuki3 <joekariuki3@gmail.com>
2023-09-15 19:00:42 +0300

    All states via SQLAlchemy correction

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,14 +9,15 @@ if __name__ == '__main__':
     '''
     execute only when run as main
     '''
-    userName = sys.argv[1]
-    passWord = sys.argv[2]
-    databaseName = sys.argv[3]
+    if (len(sys.argv) == 4):
+        userName = sys.argv[1]
+        passWord = sys.argv[2]
+        databaseName = sys.argv[3]
 
-    engine = create_engine("mysql://{}:{}@localhost/{}".format(userName,
-                           passWord, databaseName))
-    results = engine.execute("SELECT * FROM states ORDER BY id")
-    states = results.fetchall()
+        engine = create_engine("mysql://{}:{}@localhost/{}".format(userName,
+                               passWord, databaseName))
+        results = engine.execute("SELECT * FROM states ORDER BY id")
+        states = results.fetchall()
 
-    for state in states:
-        print(f"{state[0]}: {state[1]}")
+        for state in states:
+            print(f"{state[0]}: {state[1]}")
