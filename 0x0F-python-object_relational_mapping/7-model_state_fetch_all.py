7d1da95a08ed7c5900536d9a2ff168808d7a59e0
joekariuki3 <joekariuki3@gmail.com>
2023-09-15 18:55:05 +0300

    All states via SQLAlchemy correction

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 import sys
 if __name__ == '__main__':
+    '''
+    execute only when run as main
+    '''
     userName = sys.argv[1]
     passWord = sys.argv[2]
     databaseName = sys.argv[3]
