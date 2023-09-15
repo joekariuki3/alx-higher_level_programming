997345b6440622a575e34ab10341f844d2e0822e
joekariuki3 <joekariuki3@gmail.com>
2023-09-15 10:17:06 +0300

    Filter states correction 3

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,23 +9,24 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    if (len(sys.argv) == 4):
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-        # connect to the database
-        db = MySQLdb.connect(host='localhost', port=3306, user=username,
-                             passwd=password, db=database)
+    # connect to the database
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
 
-        # create a cursor to use to read info in the database
-        cur = db.cursor()
-        cur.execute('''SELECT * FROM states WHERE
-                    name LIKE 'N%' ORDER BY states.id ASC''')
+    # create a cursor to use to read info in the database
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM states WHERE
+                name LIKE 'N%' ORDER BY states.id ASC''')
 
-        # select all info from the table state
-        statesList = cur.fetchall()
+    # select all info from the table state
+    statesList = cur.fetchall()
 
-        # print states
-        for state in statesList:
-            print(state)
+    # print states
+    for state in statesList:
+        print(state)
+    cur.close()
+    db.close()
