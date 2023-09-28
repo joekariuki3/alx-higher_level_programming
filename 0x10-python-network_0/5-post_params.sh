de7b0e8f141fb167115093ad47d1c6c18b9c3c8f
joekariuki3 <joekariuki3@gmail.com>
2023-09-28 20:21:31 +0300

    cURL POST parameters correction

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes a url display body send post with variables curl s silent -X method (POST) -d data to pass using POST in body
-curl -s -X POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
