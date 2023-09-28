06a8d1241d4be8a64ea12db377209fbc96dc9b76
joekariuki3 <joekariuki3@gmail.com>
2023-09-28 15:12:38 +0300

    cURL only method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes url, send a request and display Allowed methods curl s silent I header L location(redirection) -X method(OPTION) grep i case
-curl -sILX OPTIONS "$1" | grep -i 'Allow:'| awk '{print $2, $3, $4}' 
+curl -sILX OPTIONS "$1" | grep -i 'Allow: '| awk  -F ':' '{ print $2 }' 
