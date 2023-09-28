18f207cfd17949b6099d16f1a3672cb958885314
joekariuki3 <joekariuki3@gmail.com>
2023-09-28 12:48:05 +0300

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes url, send a request and display the body curl s silent  L location(redirection)
+# script that takes url, send a request and display the body curl s silent  L location(redirection) X for method (GET,POST...)
 curl -sL -X GET "$1"
