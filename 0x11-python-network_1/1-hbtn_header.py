0639c835a7e9388697b27b01128fc692ead1fc6d
joekariuki3 <joekariuki3@gmail.com>
2023-09-29 15:41:37 +0300

    Response header value #0 correction

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,17 +8,17 @@ i.e : urllib.request
 import sys
 import urllib.request as request
 
-argv = sys.argv
-if len(argv) == 2:
+if __name__ == '__main__':
     """
-    if first argument is passed save it as url
+    execute only if run as main
     """
-    url = argv[1]
+    argv = sys.argv
+    if len(argv) == 2:
+        """
+        if first argument is passed save it as url
+        """
+        url = argv[1]
 
-    if __name__ == '__main__':
-        """
-        execute only if run as main
-        """
         with request.urlopen(url) as response:
             headerValue = response.getheader('X-Request-Id')
         print(headerValue)
