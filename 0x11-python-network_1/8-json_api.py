cc15c785ddf8fc29a6acd7fc2c831a50d6588883
joekariuki3 <joekariuki3@gmail.com>
2023-09-29 14:41:05 +0300

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+import required modules
+requests
+sys
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    """
+    only execute when run as main
+
+    script to get json data depending on the passed value
+    using post method
+    """
+    argv = sys.argv
+    latter = ""
+    if len(argv) == 2:
+        latter = argv[1]
+    values = {'q': latter}
+    url = 'http://0.0.0.0:5000/search_user'
+    resp = requests.post(url, data=values)
+    jsonData = resp.json()
+    if len(jsonData) == 0:
+        print("No result")
+    elif len(jsonData) > 0:
+        print(f"[{jsonData['id']}] {jsonData['name']}")
+    else:
+        print("Not a valid JSON")        
