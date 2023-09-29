2da586a1dc76f183c6e3f966cc078c95eec1a5c2
joekariuki3 <joekariuki3@gmail.com>
2023-09-29 20:03:11 +0300

    Error code #0 Correction 2

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 
 """
-import required modules.
-i.e : urllib.request
-      urllib.error
-      sys
+import required modules.i.e : urllib.request, urllib.error, sys
 """
 import sys
 import urllib.request as request
 import urllib.error as error
 
-argv = sys.argv
-if len(argv) == 2:
+if __name__ == '__main__':
     """
-    if first argument is passed save it as url
+    execute only if run as main
+    script that takes in a URL,
+    sends a request to the URL and displays
+    the body of the response (decoded in utf-8).
     """
-    url = argv[1]
+    argv = sys.argv
+    if len(argv) == 2:
+        """
+        if first argument is passed save it as url
+        """
+        url = argv[1]
 
-    if __name__ == '__main__':
-        """
-        execute only if run as main
-        """
         try:
             with request.urlopen(url) as response:
                 data = response.read()
