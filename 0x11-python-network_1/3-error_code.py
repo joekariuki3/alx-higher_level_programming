c8cbba63045b856f94e57ef02a723b8560c3054d
joekariuki3 <joekariuki3@gmail.com>
2023-09-29 10:54:48 +0300

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+import required modules.
+i.e : urllib.request
+      sys
+"""
+import sys
+import urllib.request as request
+import urllib.error as error
+
+argv = sys.argv
+if len(argv) == 2:
+    """
+    if first argument is passed save it as url
+    """
+    url = argv[1]
+
+    if __name__ == '__main__':
+        """
+        execute only if run as main
+        """
+        try:
+            with request.urlopen(url) as response:
+                data = response.read()
+            dataString = data.decode('utf-8')
+            print(dataString)
+        except error.URLError as e:
+            errorCode = e.getcode()
+            print(f"Error code: {errorCode}")
