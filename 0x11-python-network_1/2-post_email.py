a46eb9b9a29f134d12d1c69ce0f1b013c04e8c99
joekariuki3 <joekariuki3@gmail.com>
2023-09-29 19:59:53 +0300

    POST an email #0 Correction 4

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
+"""
+import required modules: urllib(request, parse), sys
+"""
 import urllib.request as request
 import urllib.parse as parse
 import sys
 
-
 if __name__ == '__main__':
     """
-    if executed as main
+    execute only if run as main
     script that takes in a URL and an email
     sends a POST request to the passed URL with the email
     as a parameter, and displays the body of the
@@ -22,16 +24,24 @@ if __name__ == '__main__':
         url = argv[1]
         email = argv[2]
 
-        # creat a dictionary of data to pass to server
+        """
+        creat a dictionary of data to pass to server
+        """
         values = {'email': email}
 
-        # format the data as required to be passed to the server
+        """
+        format the data as required to be passed to the server
+        """
         valuesEncoded = parse.urlencode(values)
 
-        # convert the data drom string to bytes
+        """
+        convert the data drom string to bytes
+        """
         valuesBytes = valuesEncoded.encode('ascii')
 
-        # attach the data to the url
+        """
+        attach the data to the url
+        """
         finalUrl = request.Request(url, valuesBytes)
 
         with request.urlopen(finalUrl) as response:
