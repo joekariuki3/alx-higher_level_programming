6c104f223993c521e9615fbc7d749a04da37e7e7
joekariuki3 <joekariuki3@gmail.com>
2023-09-29 19:54:59 +0300

    Response header value #0 Correction6

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
 """
-import required modules.
-i.e : urllib.request
-      sys
+import required modules.i.e : urllib.request, sys
 """
 import urllib.request
 import sys
 
 if __name__ == '__main__':
     """
-    execute only if
-    run as main
+    execute only if run as main
+    script that takes in a URL, sends a
+    request to the URL and displays the value
+    of the X-Request-Id variable found in
+    the header of the response.
     """
     argv = sys.argv
     if len(argv) == 2:
