b6c35dc7a4b5f6b857127ba680b0e50112c08517
joekariuki3 <joekariuki3@gmail.com>
2023-11-01 15:50:41 +0300

    No jQuery-document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// update text color of header to red #FF0000
+
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
